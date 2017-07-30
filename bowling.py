7809ccf91582c36478b89b016e40e371ff9d2fa4
gabriellaZana <zanagabriella@gmail.com>
2017-07-30 21:35:34 +0200

    changed variable name

--- bowling.py
+++ bowling.py
@@ -2,28 +2,28 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
+    for line in range(len(game)):
+        if game[line] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+            result += get_value(game[line])
+        if frame < 10 and get_value(game[line]) == 10:
+            if game[line] == '/':
+                result += get_value(game[line+1])
+            elif game[line] == 'X' or game[line] == 'x':
+                result += get_value(game[line+1])
+                if game[line+2] == '/':
+                    result += 10 - get_value(game[line+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result += get_value(game[line+2])
+        last = get_value(game[line])
         if not in_first_half:
             frame += 1
         if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[line] == 'X' or game[line] == 'x':
             in_first_half = True
             frame += 1
     return result
