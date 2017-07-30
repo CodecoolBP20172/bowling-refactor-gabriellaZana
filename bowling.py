a473f719952d1ec4df493d9668fa5604b51575f6
gabriellaZana <zanagabriella@gmail.com>
2017-07-30 21:52:00 +0200

    using upper instead of writing both xs

--- bowling.py
+++ bowling.py
@@ -32,7 +32,7 @@ def score(game):
 def get_value(char):
     if char.isdigit():
         return int(char)
-    elif char == 'X' or char == 'x':
+    elif char.upper() == 'X':
         return 10
     elif char == '/':
         return 10
